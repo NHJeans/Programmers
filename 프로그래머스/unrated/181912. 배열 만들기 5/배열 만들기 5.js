71fb57de8e910944abc490e4b38a6b2496a49c2c
NHJeans <110883544+NHJeans@users.noreply.github.com>
2023-08-19 10:16:25 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.02 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -3,10 +3,6 @@ function solution(intStrs, k, s, l) {
     for (let i = 0; i < intStrs.length; i++) {
         let str = intStrs[i];
         let substr = str.substr(s, l);
-        let num = parseInt(substr);
-        if (num > k) {
-            answer.push(num);
-        }
     }
     return answer;
 }
