a711c4134201680c46774c382cc203cf77285953
NHJeans <110883544+NHJeans@users.noreply.github.com>
2023-08-20 22:58:39 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,4 @@
 function solution(phone_number) {
-    let count = ([...phone_number].slice(0, -4).length)
-    let a = "*".repeat(count) + [...phone_number].slice(-4).join("")
-    return a
+    var answer = '';
+    return answer;
 }
