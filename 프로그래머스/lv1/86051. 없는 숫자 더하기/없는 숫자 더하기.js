03577e9ca1184461b4d2c0af466fbbad22bfd13b
NHJeans <110883544+NHJeans@users.noreply.github.com>
2023-08-21 13:33:42 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,4 @@
 function solution(numbers) {
-    let answer = 0;
-    for(let i = 0; i <= 9; i++) {
-        if(!numbers.includes(i)) answer += i;
-    }
+    var answer = -1;
     return answer;
 }
