5a704fe4817e06cfe4d9e58f221779594f3fae7b
NHJeans <110883544+NHJeans@users.noreply.github.com>
2025-05-31 21:45:39 +0900

    [level 0] Title: 주사위의 개수, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int[] box, int n) {
+        int width = box[0] / n;
+        int depth = box[1] / n;
+        int height = box[2] / n;
+        return width * depth * height;
+    }
+}
